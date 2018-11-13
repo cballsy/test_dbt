de4ab9f0965a4007c7b7982e3fd3c18bd58ee993
Chris Ballment <chris.ballment@gmail.com>
2018-11-14 10:20:17 +1100

    Added tests for models - and included tags for models

--- models/base/customer.sql
+++ models/base/customer.sql
@@ -1,19 +1,8 @@
--- select
---     c_custkey as customer_key,
---     c_name as customer_name,
---     c_address as customer_address,
---     c_nationkey as nation_key,
---     c_phone as customer_telephone,
---     c_acctbal as customer_account_balance,
---     c_mktsegment as customer_market_segment,
---     c_comment as customer_comments,
---     case
--- 		when c_acctbal > 0 then 'in credit'
--- 		when c_acctbal = 0 then 'account in order'
--- 		else 'payment owing'
--- 	end as customer_account_state
--- from
---     {{ var ('base.customer') }}
 
-select 'dummy' as dummy_field, *
-from {{ var ('base.customer') }}
+select c.*,
+    case
+		when c_acctbal > 0 then 'in credit'
+		when c_acctbal = 0 then 'account in order'
+		else 'payment owing'
+	end as c_accountstate
+from {{ var ('base.customer') }} c
--- models/transform/customer_xf.sql
+++ models/transform/customer_xf.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='ephemeral') }}
+
 with customer as (
 	select * from {{ ref('customer') }}
 ), nation as (
@@ -7,3 +9,5 @@ select *
 from customer c
   inner join nation n
     on c.c_nationkey = n.n_nationkey
+		-- make it fail testing
+		--on c.c_nationkey = c.c_nationkey

0a90b99d6dc842d49abe633c1033ffcb8816b7cc
Chris Ballment <chris.ballment@gmail.com>
2018-11-13 15:05:02 +1100

    Initial commit.

---
+++ models/base/customer.sql
@@ -0,0 +1,16 @@
+select
+    c_custkey as customer_key,
+    c_name as customer_name,
+    c_address as customer_address,
+    c_nationkey as nation_key,
+    c_phone as customer_telephone,
+    c_acctbal as customer_account_balance,
+    c_mktsegment as customer_market_segment,
+    c_comment as customer_comments,
+    case
+		when c_acctbal > 0 then 'in credit'
+		when c_acctbal = 0 then 'account in order'
+		else 'payment owing'
+	end as customer_account_state
+from
+    {{ var ('base.customer') }}
